25cb7a60e7e32a945cc5a741d8f35d414be6f5c1
Rushi-16 <r.anjanvatikar@gmail.com>
2022-05-31 18:29:34 +0530

    initial commit

---
+++ src/com/cts/Main.java
@@ -0,0 +1,11 @@
+package com.cts;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Wlcome to project");
+		System.out.println("Hope you are able to follow?");
+
+	}
+
+}
