edf5ff724702c35d962805536afc8bfcea89ca7d
Rushi-16 <r.anjanvatikar@gmail.com>
2022-05-31 18:52:14 +0530

    first branch 'iss1' commit

--- src/com/cts/Main.java
+++ src/com/cts/Main.java
@@ -1,10 +1,15 @@
 package com.cts;
 
+import com.cts.service.SomeService;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Wlcome to project");
 		System.out.println("Hope you are able to follow?");
+		
+		SomeService ss= new SomeService();
+		System.out.println(ss.findLength("program"));
 
 	}
 
